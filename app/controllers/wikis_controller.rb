c1bb14622481a65966094cdf5bdd08fa92d1ade5
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2018-01-30 15:04:26 +0000

    wiki index complete

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,2 +1,9 @@
 class WikisController < ApplicationController
+    def index
+        @wikis = Wiki.all
+    end
+
+    def show
+        
+    end
 end
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
+require "random_data"
 
 RSpec.describe WikisController, type: :controller do
+    let(:my_user) { User.create!(email: RandomData.random_email, password: RandomData.random_sentence) }
+    let(:wiki) { Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false, user: my_user) }
+    
+    describe 'GET #index' do
+        it 'assigns all the wikis to @wikis' do
+            get :index
+            expect(assigns(:wikis)).to eq([wiki]) #because Wiki.all returns an array   
+        end
+
+        it 'returns http get success' do
+            get :index
+            expect(response).to have_http_status(:success)
+        end
+
+        it 'specifically renders the index view' do
+            get :index
+            expect(response).to render_template(:index)
+        end
+    end
 
 end
