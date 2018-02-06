3102213ed60e1576d5be1cb811a8dc86757a56b9
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2018-02-06 11:58:40 +0000

    sign up premium and sign up standard

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < Devise::RegistrationsController
+    protected
+  
+    def after_inactive_sign_up_path_for(resource)
+        if params[:commit] == "Sign up for Premium Account"
+            redirect = wikis_path
+            return redirect if redirect.present?
+        else
+            redirect = root_path
+            return redirect if redirect.present?
+        end
+    end
+  end
